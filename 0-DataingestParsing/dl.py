63f7711a1545f4f221ba11de0c89bac8d38ebdde
raj-coding1 <rkgarai17@gmail.com>
2025-11-05 11:12:39 +0530

    dl

---
+++ 0-DataingestParsing/dl.py
@@ -0,0 +1,24 @@
+#import all the required packages
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import models,layers
+import matplotlib.pyplot as plt
+
+#load the data sets
+
+(x,y),(xt,yt) = tf.keras.datasets.mnist.load_data()
+(x,xt) = (1/255.,1/255.)
+
+#train the model
+
+model = models.Sequential([
+    layers.Flatten(input_shape = (28,28)),
+    layers.Dense(16,activation='relu'),
+    layers.Dense(10,activation='softmax')
+])
+model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', metrics= ['accuracy'])
+history= model.fit(x,y,epochs = 30, validation_data=(xt, yt))
+test_loss , test_accuracy = model.evaluate(xt,yt)
+# print(f'loss is : {} and accuracy is : {}'.format(test_loss))
+print(f'loss is : {test_loss} and accuracy is : {test_accuracy}')
